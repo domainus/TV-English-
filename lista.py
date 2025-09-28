b3ec53a3cc274439da0b5c9b83bf2ee2c752a3f0
nzo66 <nzo66.business@gmail.com>
2025-09-28 18:03:45 +0200

    Update lista.py

--- lista.py
+++ lista.py
@@ -3050,7 +3050,7 @@ def italy_channels():
 
                 if channel_id_match and channel_name_raw:
                     channel_id = channel_id_match.group(1)
-                    if channel_id == "853" and "rai 3" in channel_name_raw.lower():
+                    if channel_id == "853" and "rai 4" in channel_name_raw.lower():
                         print(f"[CORREZIONE] Canale ID {channel_id} corretto da '{channel_name_raw}' a 'CANALE 5 Italy'")
                         channel_name_raw = "CANALE 5 Italy"
                     lower_channel_name = channel_name_raw.lower()
